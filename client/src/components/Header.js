2e0aa6fec410315b165f3de114ede85c3f446923
Solidshots <seanbrent5@gmail.com>
2021-09-21 21:25:28 -0400

    header component base created

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,38 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function Header() {
+ 
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>MentorLink</Typography>
+          <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="secondary"
+              indicatorColor="secondary"
+              aria-label="app bar icons"
+            >
+              <Tab value="one" label="Your Profile"/>
+              <Tab value="two" label="Your Mentor"/>
+              <Tab value="three" label="Your Mentees"/>
+              <Tab value="four" label="Search"/>
+              <Tab value="five" label="Discussion"/>
+            </Tabs>
+          </Box>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
