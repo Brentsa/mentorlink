33306d4880053b892a23ccce98230ba332852ba8
Solidshots <seanbrent5@gmail.com>
2021-10-03 22:19:34 -0400

    sytle changes to mentor card

--- client/src/components/MemberCard.js
+++ client/src/components/MemberCard.js
@@ -10,8 +10,7 @@ import Typography from '@mui/material/Typography';
 export default function MemberCard() {
   return (
     <Card sx={{ 
-      maxWidth: 345, 
-      padding: 2, 
+      maxWidth: 345,
       display: 'flex', 
       flexWrap: 'wrap', 
       justifyContent: 'center', 
@@ -20,28 +19,38 @@ export default function MemberCard() {
         boxShadow: 6
       }
     }}>
-
-      <CardMedia
-        component="img"
-        image="http://placehold.it/200"
-        alt="placeholder image"
-        sx={{width: 200, height: 200, borderRadius: '50%', border: 1, borderColor: 'primary.main'}}
-      />
-
-      <Box component="div" sx={{width: '50%', height: '4px', backgroundColor: 'secondary.main', mt: 2}}></Box>
-
-      <CardContent sx={{display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center'}}>
-        <Typography gutterBottom variant="h4" component="div">User123456</Typography>
-        <Typography gutterBottom variant="h5" component="div">Industry</Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'center', backgroundColor: 'tertiary.main', padding: 2, borderRadius: 1, mt: 1 }}>
-          This will be the description of the member. You will be able to see things about them and learn about them. We can tell a lot about a person by their description.
-        </Typography>
-      </CardContent>
-      
-      <CardActions>
-        <Button variant="contained" color="secondary">Go to profile</Button>
-      </CardActions>
-
+      <Box sx={{
+        bgcolor: 'primary.dark', 
+        width: '100%', 
+        p:3, 
+        borderBottomLeftRadius: '50%', 
+        borderBottomRightRadius: '50%', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center'
+      }}>
+        <CardMedia
+          component="img"
+          image="http://placehold.it/200"
+          alt="placeholder image"
+          sx={{width: 200, height: 200, borderRadius: '50%'}}
+        />
+      </Box>
+    
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', p:2}}>
+        <CardContent sx={{display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center'}}>
+          <Typography gutterBottom variant="h4" component="div">User123456</Typography>
+          <Box component="div" sx={{width: '50%', height: '4px', backgroundColor: 'secondary.main', mb: 2}}></Box>
+          <Typography gutterBottom variant="h5" component="div">Industry</Typography>
+          <Typography variant="body1" color="text.secondary" sx={{ textAlign: 'center', backgroundColor: '#FFF', padding: 2, borderRadius: 1, mt: 1 }}>
+            This will be the description of the member. You will be able to see things about them and learn about them. We can tell a lot about a person by their description.
+          </Typography>
+        </CardContent>
+        
+        <CardActions>
+          <Button variant="contained" color="secondary">Go to profile</Button>
+        </CardActions>  
+      </Box>
     </Card>
   );
 }
