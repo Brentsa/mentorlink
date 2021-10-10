ac337f4f8cd912c39a696839fcf586074cadf938
Solidshots <seanbrent5@gmail.com>
2021-10-10 09:26:11 -0400

    member registration tied to back end. functional!

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -13,19 +13,25 @@ import * as Yup from 'yup';
 export default function RegisterForm(){
 
     //define a mutation that creates a new user in the back end
-    const [addUser, {error}] = useMutation(ADD_USER);
+    const [addUser] = useMutation(ADD_USER);
 
-    // async function formSubmit(event){
-    //     event.preventDefault();
-    //     try{
-    //         const addUserResponse = await addUser({variables: {member: {username, firstName, lastName, password}}})
-    //         const {token} = addUserResponse.data.addMember;
-    //         return Auth.login(token);
-    //     }
-    //     catch{
-    //         return console.log('Member not created.');
-    //     }
-    // }
+    async function formSubmit(values, setSubmitting){
+        //peel the member credentials off of the values object
+        const {username, firstName, lastName, password} = values;
+
+        //set the submission status to false
+        setSubmitting(false);
+
+        try{
+            //try adding the user via muation, save the token, and then call the front end login function to save token to localstorage
+            const addUserResponse = await addUser({variables: {member: {username, firstName, lastName, password}}})
+            const {token} = addUserResponse.data.addMember;
+            return Auth.login(token);
+        }
+        catch{
+            return console.log('Member not created.');
+        }
+    }
 
     return (
         <Formik
@@ -36,12 +42,7 @@ export default function RegisterForm(){
                 lastName: Yup.string().required("last name required").max(20, "cannot be more than 20 characters").trim(),
                 password: Yup.string().required('password required').min(8, "password must be at least 8 characters").trim()
             })}
-            onSubmit={(values, {setSubmitting}) => {
-                setTimeout(()=>{
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400)
-            }}
+            onSubmit={(values, {setSubmitting}) => formSubmit(values, setSubmitting)}
         >
             {formik => (
                 <Box
@@ -69,8 +70,8 @@ export default function RegisterForm(){
                         color="primary"
                         type="text"
                         {...formik.getFieldProps('username')}
-                        error={formik.touched.username && formik.errors.username}
-                        helperText={formik.touched.username && formik.errors.username}
+                        error={formik.touched.username && formik.errors.username ? true : false}
+                        helperText={formik.touched.username && formik.errors.username ? formik.errors.username : false}
                     />
 
                     <TextField
@@ -81,8 +82,8 @@ export default function RegisterForm(){
                         color="primary"
                         type="text"
                         {...formik.getFieldProps('firstName')}
-                        error={formik.touched.firstName && formik.errors.firstName}
-                        helperText={formik.touched.firstName && formik.errors.firstName}
+                        error={formik.touched.firstName && formik.errors.firstName ? true : false}
+                        helperText={formik.touched.firstName && formik.errors.firstName ? formik.errors.firstName : false}
                     />
 
                     <TextField
@@ -93,8 +94,8 @@ export default function RegisterForm(){
                         color="primary"
                         type="text"
                         {...formik.getFieldProps('lastName')}
-                        error={formik.touched.lastName && formik.errors.lastName}
-                        helperText={formik.touched.lastName && formik.errors.lastName}
+                        error={formik.touched.lastName && formik.errors.lastName ? true : false}
+                        helperText={formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName : false}
                     />
 
                     <TextField
@@ -105,8 +106,8 @@ export default function RegisterForm(){
                         color="primary"
                         type="password"
                         {...formik.getFieldProps('password')}
-                        error={formik.touched.password && formik.errors.password}
-                        helperText={formik.touched.password && formik.errors.password}
+                        error={formik.touched.password && formik.errors.password ? true : false}
+                        helperText={formik.touched.password && formik.errors.password ? formik.errors.password : false}
                     />
 
                     <Button 
@@ -121,136 +122,3 @@ export default function RegisterForm(){
         </Formik>
     )
 }
-
-// export default function RegisterForm(){
-
-//     //set a state of crendentials that manage the registration form text fields
-//     const [credentials, setCredentials] = useState({username: '', firstName: '', lastName: '', password: ''});
-//     const {username, firstName, lastName, password} = credentials;
-
-//     const [formError, setFormError] = useState('');
-
-//     //define a mutation that creates a new user in the back end
-//     const [addUser, {error}] = useMutation(ADD_USER);
-
-//     function handleChange(event){
-//         switch(event.target.id){
-//             case 'username':
-//                 setCredentials({...credentials, username: event.target.value});
-//                 break;
-//             case 'firstName':
-//                 setCredentials({...credentials, firstName: event.target.value});
-//                 break;
-//             case 'lastName':
-//                 setCredentials({...credentials, lastName: event.target.value});
-//                 break;
-//             case 'password':
-//                 setCredentials({...credentials, password: event.target.value});
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
-
-//     function formValidation(){
-//         if(!username || !firstName || !lastName || !password){
-//             setFormError('Please fill out all fields to create a member');
-//         }
-//         else{
-//             setFormError('');
-//         }
-//     }
-
-//     async function formSubmit(event){
-//         event.preventDefault();
-
-//         if(formError) return console.log('Member not created. Must pass validation.');
-
-//         console.log('did this happen?')
-
-//         try{
-//             const addUserResponse = await addUser({variables: {member: {username, firstName, lastName, password}}})
-//             const {token} = addUserResponse.data.addMember;
-//             return Auth.login(token);
-//         }
-//         catch{
-//             return console.log('Member not created.');
-//         }
-//     }
-
-//     useEffect(formValidation, [username, firstName, lastName, password]);
-
-//     return (
-//         <Box 
-//             component="form" 
-//             onSubmit={formSubmit}
-//             sx={{
-//                 m: 2,
-//                 p: 2,
-//                 '& .MuiTextField-root': { m: 2, width: '40ch' },
-//                 minWidth: 360,
-//                 display: 'flex',
-//                 flexDirection: 'column',
-//                 alignItems: 'center',
-//                 borderRadius: 2,
-//                 boxShadow: 3,
-//             }}
-//             noValidate
-//             autoComplete="off"
-//         >
-//             <Typography variant="h5">Register an account</Typography>
-                    
-//             <TextField
-//                 required
-//                 id='username'
-//                 label="Username"
-//                 variant="filled"
-//                 color="secondary"
-//                 value={username}
-//                 onChange={handleChange}
-//             />
-
-//             <TextField
-//                 required
-//                 id='firstName'
-//                 label="First Name"
-//                 variant="filled"
-//                 color="secondary"
-//                 value={firstName}
-//                 onChange={handleChange}
-//             />
-
-//             <TextField
-//                 required
-//                 id='lastName'
-//                 label="Last Name"
-//                 variant="filled"
-//                 color="secondary"
-//                 value={lastName}
-//                 onChange={handleChange}
-//             />
-
-//             <TextField
-//                 required
-//                 id='password'
-//                 label="Password"
-//                 variant="filled"
-//                 color="secondary"
-//                 type="password"
-//                 value={password}
-//                 onChange={handleChange}
-//             />
-
-//             {error && <Typography color="secondary" sx={{mb: 1}}>{error.message}</Typography>}
-//             {formError && <Typography color="secondary" sx={{mb: 1}}>{formError}</Typography>}
-
-//             <Button 
-//                 variant="contained"
-//                 color="secondary"
-//                 type="submit"
-//             >
-//                 Register
-//             </Button>
-//         </Box>
-//     )
-// }
