3ca955d0322aa255ff23e556799e72df5f1ecb6c
Solidshots <seanbrent5@gmail.com>
2021-10-11 20:45:54 -0400

    Authorized route component created.

---
+++ client/src/components/AuthRoute.js
@@ -0,0 +1,15 @@
+import { Route, Redirect } from "react-router";
+import Auth from "../utils/AuthService";
+
+
+export default function AuthRoute({component: Component, ...rest}){
+    return (
+        <Route
+            {...rest}
+            render={props => Auth.UserLoggedIn() 
+                ? <Component {...props} />
+                : <Redirect to={{pathname: '/login', state: {from: props.location}}} />
+            }
+        />
+    )
+}
