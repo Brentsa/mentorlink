3bd975abd3e6c1cd8d2a313958f800861b0a7558
Brentsa <seanbrent5@gmail.com>
2022-01-17 08:14:40 -0500

    fixed current page bug when visiting dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,7 +7,7 @@ import { QUERY_MEMBER } from '../utils/queries';
 import { Box } from '@mui/system';
 import Auth from '../utils/AuthService';
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loginUser, setLoggedIn } from '../redux/slices/memberSlice';
 import { switchPage } from '../redux/slices/pageSlice';
 import { isUserProfile } from '../utils/helpers';
@@ -15,6 +15,7 @@ import { isUserProfile } from '../utils/helpers';
 export default function Dashboard(){
 
     const dispatch = useDispatch();
+    const user = useSelector(state => state.members.currentUser);
     
     //peel the username off of the URL using useParams and set it to userParam
     const {username: userParam} = useParams();
@@ -29,18 +30,30 @@ export default function Dashboard(){
     const [queryCurrentUser] = useLazyQuery(QUERY_MEMBER, {
         fetchPolicy: "network-only", 
         onCompleted: data => dispatch(loginUser(data.member))
-    })
+    });
 
     //once the data has been returned, set the current member once the component has rendered
     useEffect(()=>{
         //if there is data from the lazy query, store the user's data as the current member
         if(data) setCurrentMember(data.member);
-    }, [data])
+    }, [data]);
+
+    //manage the state of the current page based on who's profile the user is visiting
+    useEffect(()=>{
+        if(currentMember && user){
+            //when arriving on the page set the current page state to your profile if your account
+            if(isUserProfile(currentMember?.username)) 
+                dispatch(switchPage("yourProfile"));
+            else if(user?.mentorGroup?.mentor?.username === currentMember?.mentorGroup?.mentor?.username)
+                dispatch(switchPage("yourMentor"));
+        }
+        else 
+            dispatch(switchPage('search'));
+
+    }, [dispatch, currentMember, user]);
 
     //after the component has rendered, set the user logged in state to true if they are logged in via Auth
     useEffect(()=>{
-        console.log(currentMember);
-
         //once the component has rendered, if the use is logged in, set the logged in state to true, user profile bool, and current user global state
         if(Auth.UserLoggedIn()){
             //set the logged in global state for the logged in user
@@ -48,15 +61,8 @@ export default function Dashboard(){
 
             //lazy query the current user
             queryCurrentUser({variables: {username: Auth.getProfile().username}});
-        } 
-
-        //when arriving on the page set the current page state to your profile if your account
-        if(isUserProfile(currentMember?.username)) 
-            dispatch(switchPage("yourProfile"));
-        else 
-            dispatch(switchPage('search'));
-
-    }, [dispatch, queryCurrentUser, currentMember, userParam])
+        }
+    }, [dispatch, queryCurrentUser, userParam])
 
     //return loading while the query executes
     if(loading) return <Box>Loading...</Box>
@@ -72,67 +78,3 @@ export default function Dashboard(){
         </Grid>
     )
 }
-
-// export default function Dashboard(){
-
-//     const dispatch = useDispatch();
-    
-//     //peel the username off of the URL using useParams and set it to userParam
-//     const {username: userParam} = useParams();
-   
-//     //compare the profile to the current logged in user and set the state to false for later use
-//     const [bIsUserProfile, setIsUserProfile] = useState(false);
-
-//     //create a state for the member of the profile
-//     const [currentMember, setCurrentMember] = useState(null);
-
-//     //query the member given by the params, we want the query to execute on every render so change the fetch policy to network only
-//     const {data, loading} = useQuery(QUERY_MEMBER, {variables: {username: userParam}, fetchPolicy: "network-only"});
-
-//     //lazy query the user, only over the network, and save their data to global state when done
-//     const [queryCurrentUser] = useLazyQuery(QUERY_MEMBER, {
-//         fetchPolicy: "network-only", 
-//         onCompleted: data => dispatch(loginUser(data.member))
-//     })
-
-//     //once the data has been returned, set the current member once the component has rendered
-//     useEffect(()=>{
-//         //if there is data from the lazy query, store the user's data as the current member
-//         if(data) setCurrentMember(data.member);
-//     }, [data])
-
-//     //after the component has rendered, set the user logged in state to true if they are logged in via Auth
-//     useEffect(()=>{
-
-//         //once the component has rendered, if the use is logged in, set the logged in state to true, user profile bool, and current user global state
-//         if(Auth.UserLoggedIn()){
-//             //set the logged in global state for the logged in user
-//             dispatch(setLoggedIn(true));
-
-//             //set the state of the profile based on the username param in the url
-//             setIsUserProfile(userParam === Auth.getProfile().username);
-
-//             //lazy query the current user
-//             queryCurrentUser({variables: {username: Auth.getProfile().username}});
-//         } 
-
-//         //when arriving on the page set the current page state to your profile if your account
-//         if(bIsUserProfile) dispatch(switchPage("yourProfile"));
-//         else dispatch(switchPage('search'));
-
-//     }, [dispatch, queryCurrentUser, bIsUserProfile, setIsUserProfile, userParam])
-
-//     //return loading while the query executes
-//     if(loading) return <Box>Loading...</Box>
-
-//     return (
-//         <Grid container spacing={4}>
-//             <Grid item xs={12} md={6}>
-//                 {currentMember ? <ProfileMember member={currentMember} setMember={setCurrentMember} bIsUserProfile={bIsUserProfile}/> : 'User not found'}
-//             </Grid>
-//             <Grid item xs={12} md={6}>
-//                 <ProfileMentor member={currentMember} setMember={setCurrentMember} bIsUserProfile={bIsUserProfile}/>
-//             </Grid>
-//         </Grid>
-//     )
-// }
